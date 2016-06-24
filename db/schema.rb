0650689ef808c43813449ae28ccaf12e165345a5
ken-wong <wangkun025@gmail.com>
2016-06-24 17:35:30 +0800

    Drop admin

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160618051529) do
+ActiveRecord::Schema.define(version: 20160624092922) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace",     limit: 255
@@ -45,13 +45,6 @@ ActiveRecord::Schema.define(version: 20160618051529) do
 
   add_index "admin_users", ["email"], name: "index_admin_users_on_email", unique: true, using: :btree
   add_index "admin_users", ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true, using: :btree
-
-  create_table "admins", force: :cascade do |t|
-    t.string   "name",            limit: 255
-    t.string   "password_digest", limit: 255
-    t.datetime "created_at",                  null: false
-    t.datetime "updated_at",                  null: false
-  end
 
   create_table "products", force: :cascade do |t|
     t.string   "title",       limit: 255
