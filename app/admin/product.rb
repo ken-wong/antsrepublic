3ec518218cbada22141a0d86a8453bee4325f776
dreamlinx <dreamlinx@gmail.com>
2016-06-26 09:36:29 +0800

    qiniu carrierwave

--- app/admin/product.rb
+++ app/admin/product.rb
@@ -2,4 +2,40 @@ ActiveAdmin.register Product do
 
 	permit_params :title, :client_name, :ref_price, :category, :description, :avatar, :main_media
 
+	qiniu_deal = '&imageView2/2/w/200/h/200'
+
+	form do |f|
+		f.semantic_errors
+		f.input :title
+		f.inputs I18n.t('activerecord.attributes.product.avatar'), :multipart => true do
+			f.input :avatar, as: :file, hint: image_tag(f.object.avatar.url + qiniu_deal)
+			f.input :avatar_cache, as: :hidden 
+		end
+		f.inputs I18n.t('activerecord.attributes.product.main_media'), :multipart => true do
+			f.input :main_media, as: :file, hint: image_tag(f.object.main_media.url)
+			f.input :main_media_cache, as: :hidden 
+		end
+		
+		f.input :client_name
+		f.input :ref_price
+		f.input :category
+		f.input	:description
+
+		f.actions
+	end
+
+	show do
+		attributes_table do
+			row :title
+			row I18n.t('activerecord.attributes.product.avatar') do
+				image_tag product.avatar.url + qiniu_deal
+			end
+
+			row :main_media
+			row :client_name
+			row :ref_price
+			row :category
+			row	:description
+		end
+	end
 end
---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,11 @@
+::CarrierWave.configure do |config|
+  config.storage             = :qiniu
+  config.qiniu_access_key    = "93vlzlK9UlO6UhZaVlrZ4RyVanIv5f1meAX_ofK2"
+  config.qiniu_secret_key    = "7UGe9arh_jrxTQGa1WLba3D8xDZ-FbXOJSVYAJt7"
+  config.qiniu_bucket        = "ants"
+  config.qiniu_bucket_domain = "o9ctas8fi.bkt.clouddn.com"
+  config.qiniu_bucket_private= true #default is false
+  config.qiniu_block_size    = 4*1024*1024
+  config.qiniu_protocal      = "http"
+
+end
