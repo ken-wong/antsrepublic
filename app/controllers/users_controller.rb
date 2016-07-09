6f19ab20b0a0cddeaff9da8c06f2443a7c3a3f2d
ken-wong <wangkun025@gmail.com>
2016-07-09 16:52:58 +0800

    Fix partial render issue

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -36,6 +36,10 @@ class UsersController < ApplicationController
   def choose
   end
 
+  def dashboard
+    @user = User.find(params[:id])
+  end
+
   private
     def user_params
       params.require(:user).permit(
