5861f3dba030615a5354bcd0e9339e70b21a3ab2
dreamlinx <dreamlinx@gmail.com>
2016-07-22 17:25:53 +0800

    profile validate for phone

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,3 +1,8 @@
 class Profile < ActiveRecord::Base
 	belongs_to :user
+	validates :phone, presence: true
+
+	mount_uploader :verify_img1, AvatarUploader
+	mount_uploader :verify_img2, AvatarUploader
+	mount_uploader :verify_img3, AvatarUploader
 end
