bd584e87c792f272cd873e098822b6529f574d30
dreamlinx <dreamlinx@gmail.com>
2016-08-04 17:13:59 +0800

    ants

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,5 +2,6 @@ class WelcomeController < ApplicationController
   def index
     @q = Product.ransack(category_eq: params[:category])
     @products = @q.result
+    @queens = User.with_role(:queen)
   end
 end
