bfeea12ddb9614f54cf19e38fb1367973f4e0a04
dreamlinx <dreamlinx@gmail.com>
2016-08-12 01:20:23 +0800

    fix avatar upload url

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -1,3 +1,13 @@
 class Plan < ActiveRecord::Base
 	belongs_to :need
+
+	state_machine :state, :initial => :'open' do
+    event :close! do
+      transition [nil, :'open'] => :'closed'
+    end
+
+    event :reopen! do
+    	transition [nil, :'closed'] => :'open'
+    end
+  end
 end
