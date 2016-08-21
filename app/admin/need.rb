c5477e827947b133ffffbf1e3e4d00776ef9d5b8
dreamlinx <dreamlinx@gmail.com>
2016-08-21 10:16:47 +0800

    fix status

--- app/admin/need.rb
+++ app/admin/need.rb
@@ -20,7 +20,7 @@ ActiveAdmin.register Need do
 			f.input	:description
 			f.input :tag_list, hint: '请使用小写的逗号分割不同标签', input_html:  {value: f.object.tag_list.to_s}
 			f.input :queen_id, as: :select, collection: User.with_role(:queen).map{|u| ["#{u.name}|#{u.email}", u.id]}
-			f.input :state, collection: ['等待审核', '审核拒绝', '寻找蚁后', '提交计划', '项目终止', '项目完成', '我的案例'] 
+			f.input :state, collection: ['等待审核', '审核拒绝', '寻找蚁后', '提交计划', '等待甲方', '乙方执行','项目终止', '项目完成', '我的案例'] 
 		end
 		f.actions
 	end
