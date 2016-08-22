3a47835c78f83bcb6c1515469f6faa839f2d9a3f
dreamlinx <dreamlinx@gmail.com>
2016-08-22 22:30:16 +0800

    fix jquery upload bug

--- app/models/queen_work.rb
+++ app/models/queen_work.rb
@@ -1,15 +1,13 @@
 class QueenWork < Product
  default_scope { where state: '我的案例'}
  belongs_to :queen
-
+ include Rails.application.routes.url_helpers
  def to_jq_upload
     {
       "name" => read_attribute(:avatar),
       "size" => avatar.size,
       "url" => avatar.url,
-      "thumbnail_url" => avatar.thumb.url,
-      "delete_url" => picture_path(:id => id),
-      "delete_type" => "DELETE" 
+      "thumbnail_url" => avatar.small_url
     }
  end
 end
