478085725798399980870f99d3e47ec95706551c
ken-wong <wangkun025@gmail.com>
2016-09-13 14:29:41 +0800

    Remove assets in public folder

--- public/assets/welcome-7a3da5d22b6b2edf3bea5cd7d61811a43981eb6f3ff6077f4e2c7029c0f47996.js
+++
@@ -1,1 +0,0 @@
-$(function(){$.scrollUp({scrollDistance:600,scrollText:"\u8fd4\u56de\u9876\u90e8"}),$(".homepageCarousel.owl-carousel").owlCarousel({stagePadding:300,loop:!0,margin:50,nav:!0,dots:!0,navText:["",""],responsive:{0:{items:1}}}),$(".case-carousel.owl-carousel").owlCarousel({loop:!1,margin:10,nav:!0,dots:!1,navText:["<",">"],responsive:{0:{items:3}}})});
