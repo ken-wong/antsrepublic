c60af5080e865978e8359bcdf2b7f5f384d3a4a5
dreamlinx <dreamlinx@gmail.com>
2016-09-14 17:44:34 +0800

    fix2 qiniu

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -44,7 +44,7 @@ class AvatarUploader < CarrierWave::Uploader::Base
 
   def small_url 
     if self.url 
-      if %w{mp4 avi}.include?(File.extname(self.url).downcase)
+      if %w{.mp4 .avi}.include?(File.extname(self.url).downcase)
         self.url + '?vframe/png/offset/3/w/200/h/200'
       else
         self.url  + '?imageView2/1/w/200/h/200' 
