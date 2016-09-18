e1e1a1c0f73e8626340d197f8c3b9de73e57fae1
yuanshengchao <1217398472@qq.com>
2016-09-18 17:32:06 +0800

    修改了首页ajax的提交方式，但是该版本目前的url地址还是错误的

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -24,6 +24,27 @@ $(function(){
 	            items:3
 	        }
 	    }
+	});
+
+	// 修改首页的显示效果是ajax查询
+	var $productList=$(".product-list-jack");
+	$productList.click(function(e){
+		e.preventDefault();
+		var indexValue=$productList.index(this);
+		$productList.removeClass("active_class");
+		$productList.eq(indexValue).addClass("active_class");
+		$.ajax({
+			url:"/api/queen_works.json",
+			type:"post",
+			data:{category:'效果图'},
+			dataType:"json",
+			success:function(data){
+				alert("success");
+			},
+			error:function(e){
+				console.log(e);
+			}
+		});
 	})
 
 })
