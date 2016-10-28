e64d60ed0426468971485dd759733c264efabd04
yuanshengchao <1217398472@qq.com>
2016-10-28 23:47:51 +0800

    完成首页的轮播通过ajax进行获取数据显示

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -35,7 +35,7 @@ $(function(){
     });
 
 
-	// 修改首页的显示效果是ajax查询
+	// 修改首页的案例显示效果是ajax查询
 	var $productList=$(".product-list-jack");
 	$productList.click(function(e){
 		e.preventDefault();
@@ -45,15 +45,63 @@ $(function(){
 		$.ajax({
 			url:"/api/queen_works.json",
 			type:"GET",
-			data:{category:'效果图'},
+			data:{category:$productList.eq(indexValue).html()},
 			dataType:"json",
 			success:function(data){
-				// alert("success");
+				console.log(data.queen_works);
+				$("div.owl-stage").css({
+					"width":"100%",
+					"min-height":"550px",
+					"line-height":"550px",
+					"transform": "translate3d(0px, 0px, 0px)",
+				});
+				if(data.queen_works.length){
+					$("div.owl-stage").html("");
+					for (var i = 0; i< data.queen_works.length; i++) {
+						 var str='<div class="owl-item active" style="width: 458.333px; margin-right: 10px;"><div class="item">'
+	            				+'<div class="product">'
+	              					+'<div class="productBlock">'
+	              						+'<a href="/queen_works/1">'
+	                						+'<div class="product-image" style="background-image:url('+data.queen_works[i].avatar_small_url+')">'
+	                						+'</div>'
+	    								+'</a>'
+	                					+'<div class="caption bordered">'
+		                  					+'<h3>'
+		                    					+'<a href="/queen_works/1">'+data.queen_works[i].title+'</a>'
+		                  					+'</h3>'
+		                  					+'<div class="description">'
+			                    				+'<p>'
+			                        				+'<b>委托客户:</b>'+data.queen_works[i].client_name
+			                    				+'</p>'
+			                    				+'<p>'
+			                        				+'<b>参考价格:</b>'+data.queen_works[i].ref_price
+			                    				+'</p>'
+			                    				+'<p>'
+			                      					+'<span class="glyphicon glyphicon-heart"></span>'
+			                      					+'<span>0</span>'
+			                      					+'<span class="glyphicon glyphicon-share"></span>'
+			                      					+'<span>0</span>'
+			                    				+'</p>'
+		                  					+'</div>'
+	                					+'</div>'
+	              					+'</div>'
+	            				+'</div>'
+	            			+'</div>';
+	            		$("div.owl-stage").append(str);
+					}
+					$(".owl-prev,.owl-next").show();
+				}else{
+					$("div.owl-stage").css({
+					"text-align":"center"
+				});
+					$(".owl-prev,.owl-next").hide();
+					$("div.owl-stage").html("该类型的案例还是路上哦！！");
+				}
 			},
 			error:function(e){
 				console.log(e);
 			}
 		});
-	})
+	});
 
-})
+});
