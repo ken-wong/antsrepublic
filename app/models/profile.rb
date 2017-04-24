ee293ab425c2cbe6c31e8428fb1ddc86256faff1
lvwxx <365881100@qq.com>
2017-04-24 10:37:21 +0800

    修改了注册成功页面文案和注册为委托方页面

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -2,7 +2,8 @@ class Profile < ActiveRecord::Base
 	belongs_to :user
 	validates :phone, presence: true
 	validates :verify_img2, presence: true
-	
+	validates :verify_img1, presence: true
+
 	mount_uploader :verify_img1, AvatarUploader
 	mount_uploader :verify_img2, AvatarUploader
 	mount_uploader :verify_img3, AvatarUploader
