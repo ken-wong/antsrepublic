5f8eabd099039e091ff00465cb611bfa57d73832
tomlelouch <tomlelouch@gmail.com>
2017-05-05 09:38:47 +0800

    在后台的需求管理中，也要可以看到所有任务

--- app/admin/task.rb
+++ app/admin/task.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Task do
-menu priority: 3
+  menu priority: 3
+
+  belongs_to :need
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
