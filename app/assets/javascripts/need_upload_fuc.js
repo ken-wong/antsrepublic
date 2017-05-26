c23166d5806666ca7382195b77eacbd9e233cc1c
lvwxx <365881100@qq.com>
2017-05-26 10:24:00 +0800

    上传资料显示上传中

--- app/assets/javascripts/need_upload_fuc.js
+++ app/assets/javascripts/need_upload_fuc.js
@@ -2,6 +2,7 @@ $(function(){
   $('.need_img_upload').fileupload({
     dataType: 'script',
     submit: function(e, data) {
+       $('.img_list').prepend('<div class="uploading">上传中</div>')
     },
     add: function(e, data) {
       data.submit(e, data);
