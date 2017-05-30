a28d4d3c90bf3ee70aab584ded77363a5d0c7be5
tomlelouch <tomlelouch@gmail.com>
2017-05-31 00:07:24 +0800

    搜索栏添加搜索功能

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,14 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  before_filter :set_global_search
   protect_from_forgery with: :exception
   include ApplicationHelper
-  
+
+  def set_global_search
+    @q = Queen.ransack(name_cont: params[:q])
+    @qw = QueenWork.ransack(title_cont: params[:q])
+  end
 
   protected
 # todo, replace devise with ken's code
