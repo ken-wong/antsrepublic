21a1c97183cbad9c6363365a5dc7cd4987a19500
tomlelouch <tomlelouch@gmail.com>
2017-12-11 14:46:58 +0800

    user test

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -2,7 +2,7 @@
 class Notifier < ApplicationMailer
   def send_notification(user, content)
     # http://localhost:3000/rails/mailers/notifier/send_notification
-    @user = user
+    @user = "user"
     @content = content
     mail :to => user.email, :subject => "蚂蚁共和消息通知", cc: "wincent@realmarkcg.com"
   end
